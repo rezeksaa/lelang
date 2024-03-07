a1ae2182e7d535688cee6e6bed24343bbe6f3c02
RezkyRezeke <156166383+rezeksaa@users.noreply.github.com>
2024-03-07 23:07:09 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lelang;
+
+/**
+ *
+ * @author mrezk
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Masyarakat masyarakat = new Masyarakat();
+        Barang barang = new Barang();
+        Lelang lelang = new Lelang();
+        
+        lelang.mulai();
+        lelang.mulai(masyarakat,barang);
+    }
+
+}
